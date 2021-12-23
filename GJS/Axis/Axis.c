1d5377a0add3e587ced6a9bca117d2507f830052
BoWei <s.rd08@outlook.com>
2021-12-23 08:21:44 +0800

    座標轉軸位置

--- GJS/Axis/Axis.c
+++ GJS/Axis/Axis.c
@@ -27,32 +27,52 @@ enum CoordinatePointPosition//座標點位置
 	CurrentPosition,
 	TargetPosition,
 	OffsetPostiton,
+	TempPostiton,
 	PositionNumber
 }ePointPosition = PositionNumber;
 enum PointName//點名稱
 {
-	X,
-	Y,
-	Z,
-	Angle,
+	_X,
+	_Y,
+	_Z,
+	_Angle,
 	PointNameNumber
 }ePointName = PointNameNumber;
+enum RangePoint//移動範圍
+{
+	Angle_Min,
+	Angle_Max,
+	Distance_Min,
+	Distance_Max,
+	RangePointNumber
+}eRangePoint = RangePointNumber;
+enum AxisPlusValue//移動資料
+{
+	PreviousPlus,
+	CurrentPlus,
+	TargetPlus,
+	AxisPlusNumber
+}eAxisPlusValue = AxisPlusNumber;
 
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
+char Msg[256];
 double ReductionRation[AxisNumber];//各軸減速比
 int MaxOutputFrequency[AxisNumber];//各軸最大輸出頻率 plus/min
 double ScrewPitch[AxisNumber];//各軸Pitch mm
 double MaxOutputSpeed[AxisNumber];//經減速機後最大速度(RPM, mm/min)
 double AxisResolution[AxisNumber];//軸輸出解析(Angle/plus, mm/plus)
+double CoordinatePoint[PositionNumber][PointNameNumber];//座標點
+double AxisPosition[AxisNumber][PositionNumber];//軸位置
+double MovableRange[AxisNumber][RangePointNumber];//移動範圍
+int AxisPlusData[AxisNumber][AxisPlusNumber];//軸移動資料
 int RunSpeedPercentage = 100;//運行速度百分比
 double SlowestSpeed = 0;//最慢速度
-double CoordinatePoint[PositionNumber][PointNameNumber];
 
 int command;
 int count;
 double _mm_per_pulse = 0.01;
-char Msg[256];
+
 int ActionInt = 0;
 
 #pragma region Custom//自定義
@@ -130,6 +150,7 @@ void Screw_Pitch(double J1_Picth, double J2_Picth, double J3_Picth, double J4_Pi
 			case J2: ScrewPitch[J2] = J2_Picth; break;
 			case J3: ScrewPitch[J3] = J3_Picth; break;
 			case J4: ScrewPitch[J4] = J4_Picth; break;
+			default: break;
 		}
 
 		if (i != 0)
@@ -179,6 +200,7 @@ void Axis_Resolution()//各軸輸出解析計算
 void Max_Output_Frequency()//最大輸出頻率計算
 {
 	printf("Max Output Frequency = [");
+	
 	for (size_t i = 0; i < AxisNumber; i++)
 	{
 		MaxOutputFrequency[i] = MaxRPM * _pulse_per_round;
@@ -196,13 +218,14 @@ void Max_Output_Speed()//經減速機後最大速度計算
 	for (size_t i = J1; i < AxisNumber; i++)
 	{
 		eSCARA = i;
-		MaxOutputSpeed[eSCARA] = (double)((MaxRPM / ReductionRation[eSCARA]) * ScrewPitch[eSCARA]);
+		MaxOutputSpeed[eSCARA] = ((double)MaxRPM / ReductionRation[eSCARA]) * ScrewPitch[eSCARA];
 		switch (eSCARA)
 		{
 			case J1: printf("%.2f RPM", MaxOutputSpeed[eSCARA]); break;
 			case J2: printf(", %.2f mm/min", MaxOutputSpeed[eSCARA]); break;
 			case J3: printf(", %.2f RPM", MaxOutputSpeed[eSCARA]); break;
 			case J4: printf(", %.2f mm/min", MaxOutputSpeed[eSCARA]); break;
+			default: break;
 		}
 	}
 	printf("]\n");
@@ -217,30 +240,143 @@ void Slowest_Speed()//最慢速
 		{
 			case J2: SlowestSpeed = MaxOutputSpeed[J2]; break;
 			case J4: if (MaxOutputSpeed[J4] < SlowestSpeed) SlowestSpeed = MaxOutputSpeed[J2]; break;
+			default: break;
 		}
 	}
 	printf("Slowest Speed = %6.f\n", SlowestSpeed);
 }
-void InitCoordinatePoint()//座標點初始化
+void PrintCoordinatePoint()//Print全部設定座標點
 {
-	printf("<====Coordinate Point Initialization====>\n");
-	for (size_t i = 0; i < PositionNumber; i++)
+	printf("New Coordinate Point >>\n");
+	for (size_t i = 0; i < PositionNumber; i++) 
 	{
 		ePointPosition = i;
+		printf("\t[%d] = [", ePointPosition);
 		for (size_t j = 0; j < PointNameNumber; j++)
 		{
-			CoordinatePoint[i][j] = 0.000;
-			switch (ePointPosition)
-			{
-				case PreviousPosition: if (j == 0) printf("Previous Position = [%.3f",CoordinatePoint[i][j]); else printf(", %.3f",CoordinatePoint[i][j]); break;
-				case CurrentPosition: if (j == 0) printf("Current Position = [%.3f",CoordinatePoint[i][j]); else printf(", %.3f",CoordinatePoint[i][j]); break;
-				case TargetPosition: if (j == 0) printf("Target Position = [%.3f",CoordinatePoint[i][j]); else printf(", %.3f",CoordinatePoint[i][j]); break;
-				case OffsetPostiton: if (j == 0) printf("Offset Postiton = [%.3f",CoordinatePoint[i][j]); else printf(", %.3f",CoordinatePoint[i][j]); break;
-			}
+			ePointName = j;
+			if(j != 0) printf(", ");
+			printf("%.6f", CoordinatePoint[ePointPosition][ePointName]);
 		}
 		printf("]\n");
 	}
 }
+void PrintAxisPosition()//Print全部軸位置
+{
+	printf("Axis Position>>\n");
+	for (size_t i = 0; i < AxisNumber; i++) 
+	{
+		eSCARA = i;
+		printf("\t[");
+		for (size_t j = 0; j < PositionNumber; j++)
+		{
+			ePointPosition = j;
+			if(j != 0) printf(", ");
+			printf("%.6f", AxisPosition[eSCARA][ePointPosition]);
+		}
+		printf("]\n");
+	}
+}
+void InitCoordinatePoint()//座標點初始化
+{
+	printf("Coordinate Point Initial......\n");
+	for (size_t i = 0; i < PositionNumber; i++)
+	{
+		ePointPosition = i;
+		for (size_t j = 0; j < PointNameNumber; j++)
+			CoordinatePoint[i][j] = 0.000;
+	}
+}
+void InitRangePoint()//移動範圍初始化
+{
+	printf("Range Point Initial......\n");
+	for (size_t i = 0; i < AxisNumber; i++)
+	{
+		eSCARA = i;
+		switch (eSCARA)
+		{
+			case J1: 
+				MovableRange[eSCARA][Angle_Min] = -180.0;
+				MovableRange[eSCARA][Angle_Max] = 180.0;
+				MovableRange[eSCARA][Distance_Min] = 473.0;
+				MovableRange[eSCARA][Distance_Max] = 473.0;
+				printf("J1 Movable Range\n\tAngle = %.4f Deg ~ %.4f Deg\n\tDistance = %.3f mm ~ %.3f mm\n", MovableRange[eSCARA][Angle_Min], MovableRange[eSCARA][Angle_Max], MovableRange[eSCARA][Distance_Min], MovableRange[eSCARA][Distance_Max]);
+				break;
+			case J2: 
+				MovableRange[eSCARA][Angle_Min] = 0.0;
+				MovableRange[eSCARA][Angle_Max] = 0.0;
+				MovableRange[eSCARA][Distance_Min] = 0.0;
+				MovableRange[eSCARA][Distance_Max] = 300.0;
+				printf("J2 Movable Range\n\tAngle = %.4f Deg ~ %.4f Deg\n\tDistance = %.3f mm ~ %.3f mm\n", MovableRange[eSCARA][Angle_Min], MovableRange[eSCARA][Angle_Max], MovableRange[eSCARA][Distance_Min], MovableRange[eSCARA][Distance_Max]);
+				break;
+			case J3: 
+				MovableRange[eSCARA][Angle_Min] = -180.0;
+				MovableRange[eSCARA][Angle_Max] = 180.0;
+				MovableRange[eSCARA][Distance_Min] = 0.0;
+				MovableRange[eSCARA][Distance_Max] = 0.0;
+				printf("J3 Movable Range\n\tAngle = %.4f Deg ~ %.4f Deg\n\tDistance = %.3f mm ~ %.3f mm\n", MovableRange[eSCARA][Angle_Min], MovableRange[eSCARA][Angle_Max], MovableRange[eSCARA][Distance_Min], MovableRange[eSCARA][Distance_Max]);
+				break;
+			case J4: 
+				MovableRange[eSCARA][Angle_Min] = 0.0;
+				MovableRange[eSCARA][Angle_Max] = 0.0;
+				MovableRange[eSCARA][Distance_Min] = 0.0;
+				MovableRange[eSCARA][Distance_Max] = 400.0;
+				printf("J4 Movable Range\n\tAngle = %.4f Deg ~ %.4f Deg\n\tDistance = %.3f mm ~ %.3f mm\n", MovableRange[eSCARA][Angle_Min], MovableRange[eSCARA][Angle_Max], MovableRange[eSCARA][Distance_Min], MovableRange[eSCARA][Distance_Max]);
+				break;
+			default: break;
+		}
+	}
+}
+void InitAxisPlusData()//軸移動資料初始化
+{
+	printf("Axis Plus Data Initial......\n");
+	for (size_t i = 0; i < AxisNumber; i++)
+		for (size_t j = 0; j < AxisPlusNumber; j++)
+			AxisPlusData[i][j] = 0;
+}
+void WriteCoordinatePoint(int PointPos, double Point_X, double Point_Y, double Point_Z, double Point_Angle)//寫入座標點
+{
+	CoordinatePoint[PointPos][_X] = Point_X;
+	CoordinatePoint[PointPos][_Y] = Point_Y;
+	CoordinatePoint[PointPos][_Z] = Point_Z;
+	CoordinatePoint[PointPos][_Angle] = Point_Angle;
+}
+void SetToTempPosition(int PointPos)//設定到動態座標點
+{
+	for (size_t i = 0; i < PointNameNumber; i++)
+		CoordinatePoint[TempPostiton][i] = CoordinatePoint[PointPos][i] - CoordinatePoint[OffsetPostiton][i];
+}
+void CalculateJ1J2J3(double AngleRad, int tPos)
+{
+	double J1J2_DistanceMin = MovableRange[J1][Distance_Min] + MovableRange[J2][Distance_Min];
+	double J1J2_DistanceMax = MovableRange[J1][Distance_Max] + MovableRange[J2][Distance_Max];
+	double PointToOriginDistance = CoordinatePoint[TempPostiton][_X] / cos(AngleRad);
+
+	printf("Point to Origin Distance: %.6f mm\n", PointToOriginDistance);
+	// Check Distance
+	if ((PointToOriginDistance >= J1J2_DistanceMin) & (PointToOriginDistance <= J1J2_DistanceMax)) 
+	{
+		AxisPosition[J1][tPos] = AngleRad;
+		AxisPosition[J2][tPos] = PointToOriginDistance - MovableRange[J1][Distance_Max];
+		AxisPosition[J3][tPos] = (CoordinatePoint[TempPostiton][_Angle] * DegToRad) - AngleRad;
+		AxisPosition[J4][tPos] = CoordinatePoint[TempPostiton][_Z];
+	}
+	else
+	{
+		printf("Point out of range!\n");
+	}
+	PrintAxisPosition();
+}
+
+void CalculateAxisPosition_SCARA(int Pos)
+{
+	SetToTempPosition(Pos);
+
+	double Angle_Radian_J1 = atan(CoordinatePoint[TempPostiton][_Y] / CoordinatePoint[TempPostiton][_X]);
+	CalculateJ1J2J3(Angle_Radian_J1, Pos);
+}
+//TempPostiton
+
 
 int ReceiveChar()//接收字符
 {
@@ -262,7 +398,6 @@ int ReceiveChar()//接收字符
 	return ReceiveChar;
 }
 #pragma endregion
-
 void Core1_Entry()
 {
 	while(true)
@@ -280,6 +415,8 @@ int main() //Main Function
 	
 	sleep_us(10000000);
 	InitCoordinatePoint();
+	InitAxisPlusData();
+	InitRangePoint();
 	printf("Degree to Radian Parameter: %.10f\nRadian to Degree Parameter: %.10f\n", DegToRad, RadToDeg);
 	printf("<====SCARA Parameter Initialization====>\n");
 	Screw_Pitch(1.0, 10.0, 1.0, 25.0);
@@ -292,27 +429,37 @@ int main() //Main Function
 	Max_Output_Frequency();
 	Max_Output_Speed();
 	Slowest_Speed();
-
-	printf("<====Raspberry Pi Pico Ready!!====>\n");
+	printf("<====Set Coordinate Point====>\n");
+	WriteCoordinatePoint(OffsetPostiton, -250.0, -250.0, 0.0, 0.0);
+	WriteCoordinatePoint(CurrentPosition, 100.1, 100.0, 0.0, 0.0);
+	WriteCoordinatePoint(TargetPosition, 111.1, 111.1, 11.0, 0.0);
+	PrintCoordinatePoint();
+	printf("<====Raspberry Pi Pico Ready====>\n");
 	multicore_launch_core1(Core1_Entry);
-
+	
     while (true)
     {
 		switch (ActionInt)
 		{
-		case 48://0
+		case '0':
 			gpio_put(LED_PIN, 0);
 			break;
-		case 49://1
+		case '1':
 			gpio_put(LED_PIN, 1);
 			sleep_us(125000);
 			gpio_put(LED_PIN, 0);
 			sleep_us(125000);
 			break;
+		case 'T':
+			CalculateAxisPosition_SCARA(CurrentPosition);
+			CalculateAxisPosition_SCARA(TargetPosition);
+			ActionInt = 0;
+			break;
 		
 		default:
 			break;
 		}
+
     }
     return 0;
 }
